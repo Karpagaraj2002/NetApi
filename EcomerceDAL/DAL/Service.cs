8057ab6d1d6f8c5a46d68a1c35b04d84c992bfaa
Karpagaraj2002 <b.karpagaraj20@gmail.com>
2024-11-24 20:19:45 +0530

    first commit

---
+++ EcomerceDAL/DAL/Service.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcomerceDAL.DAL
+{
+    internal class Service
+    {
+        public Service() { }
+
+
+        string Autherservice()
+        {
+            return "test";
+        }
+    }
+}
---
+++ Ecommerce/Controllers/MainController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using EcommerceDAL;
+using EcommerceDAL.DAL;
+using Microsoft.Extensions.Configuration;
+using static EcommerceDAL.DAL.Model;
+
+namespace Ecommerce.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class MainController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public String ConnStr = "";
+        Service obj = null;
+        public MainController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+            ConnStr = _configuration.GetConnectionString("DefaultConnection");
+            obj = new Service(configuration);
+        }
+
+        [HttpGet]
+        public AllOrderDetail allgetorder()
+        {
+            AllOrderDetail objResponse = new AllOrderDetail();
+
+            objResponse = obj.allGetOrder();
+
+            return objResponse;
+
+        }
+
+
+
+
+        
+    }
+
+    
+}
